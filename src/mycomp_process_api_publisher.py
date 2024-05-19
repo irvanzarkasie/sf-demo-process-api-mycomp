0d43bc8d1cf49c2fe275cf9ad1b95740fd769235
irvan <irvan.ramadhan@expecc.com>
2024-05-20 04:57:49 +0700

    Modify redis destination port

--- src/mycomp_process_api_publisher.py
+++ src/mycomp_process_api_publisher.py
@@ -28,7 +28,7 @@ log_dir = "{home_dir}/logs".format(home_dir=home_dir)
 # Initializing the redis instance
 r = redis.Redis(
     host='127.0.0.1',
-    port=6379,
+    port=36379,
     decode_responses=True # <-- this will ensure that binary data is decoded
 )
 COMM_CHANNEL = "mycomp-get-routes-req-channel"
