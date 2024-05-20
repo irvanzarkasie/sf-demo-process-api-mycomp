627d71247243cdd1ed8f5e3eb845d511d8757539
irvan <irvan.ramadhan@expecc.com>
2024-05-20 11:09:58 +0700

    Add logging

--- src/mycomp_process_api_publisher.py
+++ src/mycomp_process_api_publisher.py
@@ -13,6 +13,8 @@ import urllib3
 import redis
 import uuid
 from time import sleep
+import logging
+from logging.handlers import TimedRotatingFileHandler, RotatingFileHandler
 
 app = Flask(__name__)
 api = Api(app)
@@ -28,6 +30,15 @@ BACKOFF_MS = 50
 script_dir = os.path.dirname(os.path.realpath(__file__))
 home_dir = "/".join(script_dir.split("/")[:-1])
 log_dir = "{home_dir}/logs".format(home_dir=home_dir)
+
+# Initialize logger
+logpath = "{log_dir}/{api_id}.log".format(log_dir=log_dir, api_id=api_id)
+logger = logging.getLogger("{api_id}_logger".format(api_id=api_id))
+logformatter = logging.Formatter("%(message)s")
+logger.setLevel(logging.INFO)
+handler = logging.FileHandler(logpath)
+handler.setFormatter(logformatter)
+logger.addHandler(handler)
 
 # Initializing the redis instance
 r = redis.Redis(
@@ -96,8 +107,60 @@ TRANSTYPECODEMAPREVERSE = {
     "ECOM-TAXI": "9005"
 }
 
+def logRequest(request, rqid="", level="INFO"):
+  rqtimestamp = datetime.now()
+  logtype = "request"
+  apiurl = request.url
+  client_addr = request.remote_addr
+  message = "Receive request"
+  logpayload = {
+    "host": api_host,
+    "rqid": rqid,
+    "level": level,
+    "rqtimestamp": str(rqtimestamp),
+    "logtype": logtype,
+    "client_addr": client_addr,
+    "logger_name": api_id,
+    "message": message,
+    "req_url": apiurl
+  }
+  logger.info(json.dumps(logpayload))
+# end def
+
+def logResponse(response, rqid="", message="Return response", duration=0, rqtimestamp="", request={}, level="INFO", status_code=""):
+  rstimestamp = datetime.now()
+  logtype = "response"
+  logpayload = {
+    "host": api_host,
+    "rqid": rqid,
+    "level": level,
+    "rqtimestamp": str(rqtimestamp),
+    "rstimestamp": str(rstimestamp),
+    "logtype": logtype,
+    "response": response,
+    "status_code": status_code,
+    "logger_name": api_id,
+    "message": message
+  }
+  if duration != 0:
+    logpayload["duration"] = duration
+  # end if
+  if len(str(rqtimestamp)) != 0:
+    logpayload["rqtimestamp"] = str(rqtimestamp)
+  # end if
+  if len(request.remote_addr) != 0:
+    logpayload["client_addr"] = request.remote_addr
+  # end if
+  if len(request.url) != 0:
+    logpayload["req_url"] = request.url
+  # end if
+
+  logger.info(json.dumps(logpayload))
+# end def
+
 class MyCompProcApi(Resource):
     def get(self, transport_type):
+      start_timestamp = datetime.now()
       # Parse arguments
       args = request.args
       transport_code = TRANSTYPECODEMAPREVERSE.get(transport_type, "")
@@ -106,6 +169,8 @@ class MyCompProcApi(Resource):
 
       correlation_id = datetime.now().strftime("%Y%m%d%H%M%S") + str(uuid.uuid4()).replace("-","")
       msg_ts = int(datetime.now().timestamp())
+
+      logRequest(request, rqid=correlation_id)
 
       b2u_corr_id = "B2U_{correlation_id}".format(correlation_id=correlation_id)
       easycomego_corr_id = "EASYCOMEGO_{correlation_id}".format(correlation_id=correlation_id)
@@ -267,12 +332,16 @@ class MyCompProcApi(Resource):
         ]
       # end else
 
+      end_timestamp = datetime.now()
+      duration = int((end_timestamp - start_timestamp).total_seconds() * 1000)
+      logResponse(response, rqid=correlation_id, message="Return response", duration=duration, rqtimestamp=start_timestamp, request=request, level="INFO", status_code=200):
       return jsonify(resp_list)
     # end def
 # end class
 
 class MyCompProcApiDefault(Resource):
     def get(self):
+      start_timestamp = datetime.now()
       # Parse arguments
       args = request.args
       departure_code = DEPDESTCODEMAPREVERSE.get(args.get("departureCode", ""), "")
@@ -280,6 +349,8 @@ class MyCompProcApiDefault(Resource):
 
       correlation_id = datetime.now().strftime("%Y%m%d%H%M%S") + str(uuid.uuid4()).replace("-","")
       msg_ts = int(datetime.now().timestamp())
+
+      logRequest(request, rqid=correlation_id)
 
       b2u_corr_id = "B2U_{correlation_id}".format(correlation_id=correlation_id)
       easycomego_corr_id = "EASYCOMEGO_{correlation_id}".format(correlation_id=correlation_id)
@@ -409,6 +480,9 @@ class MyCompProcApiDefault(Resource):
         {"transportProvider": "T-002", "routes": b2u_resp_list},
       ]
 
+      end_timestamp = datetime.now()
+      duration = int((end_timestamp - start_timestamp).total_seconds() * 1000)
+      logResponse(response, rqid=correlation_id, message="Return response", duration=duration, rqtimestamp=start_timestamp, request=request, level="INFO", status_code=200):
       return jsonify(resp_list)
     # end def
 # end class
